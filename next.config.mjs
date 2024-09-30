4c37e2987b635fc546207dc6649d57ef6306f794
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-09-30 17:40:10 +0200

    Descripción de los cambios que hiciste

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    images: {
-      domains: ['raw.githubusercontent.com'], // Permite cargar imágenes de este dominio
-    },
-  };
-  
-  export default nextConfig;
-  
+  trailingSlash: true,
+  assetPrefix: isProd ? '/masiros/' : '', // Cambia a '/masiros/' para producción
+  basePath: isProd ? '/masiros' : '', // Cambia a '/masiros' para producción
+  output: 'export', // Para exportar la aplicación como archivos estáticos
+  images: {
+    domains: ['raw.githubusercontent.com'], // Permite cargar imágenes de este dominio
+  },
+};
+
+export default nextConfig;
