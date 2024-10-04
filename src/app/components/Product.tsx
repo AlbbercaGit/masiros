9603496223313516ed2574cb979faae320016313
AlbbercaGit <alejandroalbbercabarcelona@gmail.com>
2024-10-04 14:48:14 +0200

    final interrogante

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -4,30 +4,49 @@ import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { motion, useInView, useAnimation } from 'framer-motion'
 
+interface Product {
+  image: string;
+  title: string;
+  description: string;
+  logo: string;
+}
+
+const product: Product = {
+  image: "https://raw.githubusercontent.com/AlbbercaGit/videos/refs/heads/main/masiros/4.JPG",
+  title: "Distribuidor Exclusivo",
+  description: "MASIROS PARQUET es el único distribuidor oficial de Quick Step en Valencia. Ofrecemos productos exclusivos que no encontrarás en ningún otro lugar. Confía en la marca líder en suelos.",
+  logo: "https://raw.githubusercontent.com/AlbbercaGit/videos/refs/heads/main/logos/1.jpg"
+}
+
 export default function ProductSection() {
-  const backgroundTextRef = useRef<HTMLDivElement>(null);
   const sectionRef = useRef<HTMLElement>(null)
+  const backgroundTextRef = useRef<HTMLDivElement>(null)
   const isInView = useInView(sectionRef, { once: true, amount: 0.25 })
   const controls = useAnimation()
   const [containerWidth, setContainerWidth] = useState(0)
+  const [containerPadding, setContainerPadding] = useState(0)
   const [isHovered, setIsHovered] = useState(false)
 
   useEffect(() => {
-    const updateContainerWidth = () => {
+    const updateContainerDimensions = () => {
       if (backgroundTextRef.current) {
-        setContainerWidth(backgroundTextRef.current.scrollWidth / 2)
+        const styles = window.getComputedStyle(backgroundTextRef.current)
+        const paddingLeft = parseFloat(styles.paddingLeft)
+        const paddingRight = parseFloat(styles.paddingRight)
+        setContainerWidth(backgroundTextRef.current.scrollWidth)
+        setContainerPadding(paddingLeft + paddingRight)
       }
     }
 
-    updateContainerWidth()
-    window.addEventListener('resize', updateContainerWidth)
+    updateContainerDimensions()
+    window.addEventListener('resize', updateContainerDimensions)
 
     if (isInView) {
       controls.start("visible")
     }
 
-    return () => window.removeEventListener('resize', updateContainerWidth)
-  }, [isInView, controls]);
+    return () => window.removeEventListener('resize', updateContainerDimensions)
+  }, [isInView, controls])
 
   const contentVariants = {
     hidden: { clipPath: 'inset(0 100% 0 0)' },
@@ -52,18 +71,17 @@ export default function ProductSection() {
   }
 
   const buttonVariants = {
-    initial: {
-      scale: 1,
-    },
-    hover: {
-      scale: 1.05,
-    },
+    initial: { scale: 1 },
+    hover: { scale: 1.05 },
   }
 
   const clipPathVariants = {
     initial: { clipPath: 'inset(0 100% 0 0)' },
     hover: { clipPath: 'inset(0 0 0 0)' },
   }
+
+  const textContent = "Distribuidor Exclusivo "
+  const repeatedText = textContent.repeat(10) // Repeat the text to ensure smooth animation
 
   return (
     <section 
@@ -77,42 +95,47 @@ export default function ProductSection() {
         variants={clipPathVariants}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       />
+      
       {/* Animated background text */}
-      <motion.div 
-        ref={backgroundTextRef}
-        className="absolute top-[-25%] left-0 h-full flex items-center whitespace-nowrap"
-        style={{ width: 'max-content' }}
-        variants={backgroundTextVariants}
-        initial="hidden"
-        animate={controls}
+      <div 
+        className="absolute top-[15%] 2xl:top-[8%] xl:top-[12%] left-0 h-full overflow-hidden"
+        style={{ 
+              maskImage: 'linear-gradient(to right, transparent, black 0%, black 0%, transparent)',
+              WebkitMaskImage: 'linear-gradient(to right, transparent, black 0%, black 0%, transparent)'
+        }}
       >
-        <motion.div 
-          className="flex"
-          animate={{ 
-            x: [`0px`, `-${containerWidth}px`]
-          }}
-          transition={{ 
-            x: { 
-              repeat: Infinity,
-              repeatType: "loop",
-              duration: 15,
-              ease: "linear"
-            },
-          }}
-          style={{ width: `${containerWidth * 2}px` }}
+        <motion.div
+          ref={backgroundTextRef}
+          className="flex items-center whitespace-nowrap"
+          variants={backgroundTextVariants}
+          initial="hidden"
+          animate={controls}
         >
-          <span className="inline-block font-libre text-[10vw] text-[#817A7A]">
-          Distribuidor Exclusivo&nbsp;
-          </span>
-          <span className="inline-block font-libre text-[10vw] text-[#817A7A]">
-          Distribuidor Exclusivo&nbsp;
-          </span>
+          <motion.div
+            className="flex"
+            animate={{
+              x: [`0px`, `-${(containerWidth - containerPadding) / 2}px`],
+            }}
+            transition={{
+              x: {
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 40,
+                ease: "linear",
+              },
+            }}
+            style={{ width: `${containerWidth * 2}px` }}
+          >
+            <span className="inline-block font-libre text-[10vw] text-[#817A7A]">
+              {repeatedText}
+            </span>
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </div>
 
       {/* Content container */}
-      <motion.div 
-        className="relative z-10 max-w-[75%] mx-auto px-4 flex flex-col lg:flex-row items-center justify-evenly"
+      <motion.div
+        className="relative z-10 max-w-[75%] mx-auto px-4 flex flex-col lg:flex-row items-center md:items-end xl:items-end 2xl:items-center justify-evenly"
         variants={contentVariants}
         initial="hidden"
         animate={controls}
@@ -120,7 +143,7 @@ export default function ProductSection() {
         {/* Image */}
         <div className="lg:w-1/3 mb-8 lg:mb-0">
           <Image
-            src="https://raw.githubusercontent.com/AlbbercaGit/videos/refs/heads/main/masiros/4.JPG"
+            src={product.image}
             alt="Modern sculptural chair"
             width={550}
             height={400}
@@ -130,21 +153,30 @@ export default function ProductSection() {
 
         {/* Text content */}
         <div className="lg:w-1/3 lg:pl-12 text-left">
-          <h2 className={`text-4xl font-serif mb-4 transition-colors duration-300 ${
-            isHovered ? 'text-white' : 'text-[#333333]'
-          }`}>Distribuidor Exclusivo</h2>
-          <p className={`mb-6 transition-colors duration-300 ${
-            isHovered ? 'text-gray-300' : 'text-[#666666]'
-          }`}>
-MASIROS PARQUET es el único distribuidor oficial de Quick Step en Valencia. Ofrecemos productos exclusivos que no encontrarás en ningún otro lugar. Confía en la marca líder en suelos."          </p>
+          <h2
+            className={`text-4xl font-serif mb-4 transition-colors duration-300 ${
+              isHovered ? "text-white" : "text-[#333333]"
+            }`}
+          >
+            {product.title}
+          </h2>
+          <p
+            className={`mb-6 transition-colors duration-300 ${
+              isHovered ? "text-gray-300" : "text-[#666666]"
+            }`}
+          >
+            {product.description}
+          </p>
           <div className="flex items-center ">
             <motion.button
-              className={` py-3 px-8 text-sm relative overflow-hidden bg-transparent border transition-colors duration-300 ${
-                isHovered ? 'text-white border-white' : 'text-[#333333] border-[#333333]'
+              className={`py-3 px-8 text-sm relative overflow-hidden bg-transparent border transition-colors duration-300 ${
+                isHovered
+                  ? "text-white border-white"
+                  : "text-[#333333] border-[#333333]"
               }`}
               style={{
-                borderTopLeftRadius: '20px',
-                borderBottomRightRadius: '20px',
+                borderTopLeftRadius: "20px",
+                borderBottomRightRadius: "20px",
               }}
               variants={buttonVariants}
               initial="initial"
@@ -153,21 +185,21 @@ MASIROS PARQUET es el único distribuidor oficial de Quick Step en Valencia. Ofr
               onHoverEnd={() => setIsHovered(false)}
               transition={{ duration: 0.3 }}
             >
-              <motion.span 
+              <motion.span
                 className="relative z-20 mix-blend-difference"
-                initial={{ color: '#333333' }}
-                animate={{ color: isHovered ? 'white' : '#333333' }}
+                initial={{ color: "#333333" }}
+                animate={{ color: isHovered ? "white" : "#333333" }}
                 transition={{ duration: 0.3 }}
               >
                 Ver todo
               </motion.span>
               <motion.div
                 className={`absolute inset-0 z-10 transition-colors duration-300 ${
-                  isHovered ? 'bg-white' : 'bg-[#333333]'
+                  isHovered ? "bg-white" : "bg-[#333333]"
                 }`}
                 style={{
-                  borderTopLeftRadius: '20px',
-                  borderBottomRightRadius: '20px',
+                  borderTopLeftRadius: "20px",
+                  borderBottomRightRadius: "20px",
                 }}
                 variants={clipPathVariants}
                 initial="initial"
@@ -177,7 +209,7 @@ MASIROS PARQUET es el único distribuidor oficial de Quick Step en Valencia. Ofr
             </motion.button>
             <div className="relative w-40 h-20 ml-4">
               <Image
-                src="https://raw.githubusercontent.com/AlbbercaGit/videos/refs/heads/main/logos/1.jpg"
+                src={product.logo}
                 alt="Decorative image"
                 layout="fill"
                 objectFit="contain"
